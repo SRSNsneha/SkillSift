2528b3c0b3c0cfe2df1bd8349d0a603ce001e43c
SRSNsneha <srsneha2004@gmail.com>
2025-07-10 01:31:04 +0530

    Update feature1.js

--- docs/static/feature1.js
+++ docs/static/feature1.js
@@ -8,7 +8,7 @@ document.getElementById("upload-form").addEventListener("submit", async function
   formData.append("file", fileInput.files[0]);
 
   try {
-    const response = await fetch("http://127.0.0.1:5000/analyze_resume", {
+    const response = await fetch("https://skillsift.onrender.com/analyze_resume", {
       method: "POST",
       body: formData
     });
